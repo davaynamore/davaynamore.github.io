b4a98129c6146d3c55bcaa01be85e18f34a9f8ac
Sergey Chuprina <davaynamore@gmail.com>
2020-01-12 15:48:38 +0200

    carousel

---
+++ carousel/src/js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.owl-carousel').owlCarousel({
+		loop: true,
+		items: 3,
+		autoplay: true,
+		autoplayTimeout: 5000,
+		nav: true,
+		margin: 40,
+		dotsEach: true
+	});
+});
