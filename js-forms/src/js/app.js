c43484f60ffd4698324f8ac8729d91b53d456aa2
Sergey Chuprina <davaynamore@gmail.com>
2020-01-25 13:23:52 +0200

    contact-form added

--- js-forms/src/js/app.js
+++ js-forms/src/js/app.js
@@ -5,97 +5,128 @@
 
 // DOM
 
-console.log('string' + 'uppercase');
+// console.log('string' + 'uppercase');
 
-// String - 'string' + 1 // '2' + 2 = '22' // '2' - 2 = 0 //
-// Number - 
-// Boolean - true / false
-// Undefined
-// Object
-// Function
+// // String - 'string' + 1 // '2' + 2 = '22' // '2' - 2 = 0 //
+// // Number - 
+// // Boolean - true / false
+// // Undefined
+// // Object
+// // Function
 
-const form = document.querySelector('.form');
-const answer = document.querySelector('.answer');
+// const form = document.querySelector('.form');
+// const answer = document.querySelector('.answer');
 
-function handelSubmit(event) {
-	event.preventDefault();
+// function handelSubmit(event) {
+// 	event.preventDefault();
 
-	const data = {};
+// 	const data = {};
 
-	for(let i = 0; i < form.elements.length; i++) {
-		if (form.elements[i].name === 'submit') {
-			continue;
+// 	for(let i = 0; i < form.elements.length; i++) {
+// 		if (form.elements[i].name === 'submit') {
+// 			continue;
+// 		}
+// 		data[form.elements[i].name] = form.elements[i].value;
+// 	}
+
+// 	// Array.from(form.elements).forEach(el => {
+// 	// 	if (el.name !== 'submit') return data[el.name] = el.value;
+// 	// });
+
+// 	function getStatus(age) {
+
+// 		if (age < 0 ||  age > 150) {
+// 			return 'Incorrect value';
+// 		}
+
+// 		if (age > 0 && age <= 12) {
+// 			return 'baby';
+// 		} else if (age >= 13 && age <= 18) {
+// 			return 'teen';
+// 		} else if (age >= 19 && age <= 60) {
+// 			return 'adult';
+// 		} else {
+// 			return 'pens';
+// 		}
+// 	}
+
+// 	answer.innerText = `${data.name} ${data.secname} is ${getStatus(+data.age)}`;
+// }
+
+// form.addEventListener('submit', handelSubmit);
+
+
+// const palindromForm = document.createElement('form');
+// palindromForm.classList.add('palindrom-form');
+// document.body.appendChild(palindromForm);
+
+// const palindromInput = document.createElement('input');
+// const palindromSubmit = document.createElement('input');
+// const palindromAnswer = document.createElement('p');
+// palindromAnswer.classList.add('palindrom-form__answer');
+// palindromInput.setAttribute('name', 'number');
+// palindromInput.classList.add('palindrom-form__input');
+// palindromSubmit.setAttribute('type', 'submit');
+// palindromSubmit.classList.add('palindrom-form__submit');
+// palindromSubmit.setAttribute('value', 'Submit');
+// palindromForm.appendChild(palindromInput);
+// palindromForm.appendChild(palindromSubmit);
+// palindromForm.appendChild(palindromAnswer);
+
+// palindromForm.addEventListener('submit', function(event) {
+// 	event.preventDefault();
+
+// 	if (palindromInput.value.length !== 5) {
+// 		palindromAnswer.innerText = 'Введите пятизначное число';
+// 		return;
+// 	}
+
+// 	const isPalindrom = palindromInput.value.split('').reverse().join('') === palindromInput.value;
+
+// 	//palindromAnswer.innerText = isPalindrom ? 'Палиндром' : 'Не палиндром';
+
+// 	// if (isPalindrom === true) {
+// 	// 	palindromAnswer.innerText = 'Палиндром';
+// 	// } else if (isPalindrom === false) {
+// 	// 	palindromAnswer.innerText = 'Не палиндром';
+// 	// }
+
+// 	if (isPalindrom) {
+// 		palindromAnswer.innerText = 'Палиндром';
+// 	} else {
+// 		palindromAnswer.innerText = 'Не палиндром';
+// 	}
+
+
+// });
+
+
+const formNode = document.querySelector('.contact-form');
+
+// console.dir(formNode.elements);
+
+// for(let i = 0; i < formNode.elements.length; i++) {
+// 	console.log(formNode.elements[i]);
+// }
+
+formNode.addEventListener('change', (event) => {
+	// event.preventDefault();
+
+	console.dir(event);
+
+	const arr = [];
+	Array.from(formNode.elements).map((el) => {
+		if ( el.name !== 'checkbox' && el.name !== 'submit' ) {
+			arr.push({[el.name]: el.value});
 		}
-		data[form.elements[i].name] = form.elements[i].value;
-	}
+	});
 
-	// Array.from(form.elements).forEach(el => {
-	// 	if (el.name !== 'submit') return data[el.name] = el.value;
-	// });
-
-	function getStatus(age) {
-
-		if (age < 0 ||  age > 150) {
-			return 'Incorrect value';
-		}
-
-		if (age > 0 && age <= 12) {
-			return 'baby';
-		} else if (age >= 13 && age <= 18) {
-			return 'teen';
-		} else if (age >= 19 && age <= 60) {
-			return 'adult';
-		} else {
-			return 'pens';
-		}
-	}
-
-	answer.innerText = `${data.name} ${data.secname} is ${getStatus(+data.age)}`;
-}
-
-form.addEventListener('submit', handelSubmit);
-
-
-const palindromForm = document.createElement('form');
-palindromForm.classList.add('palindrom-form');
-document.body.appendChild(palindromForm);
-
-const palindromInput = document.createElement('input');
-const palindromSubmit = document.createElement('input');
-const palindromAnswer = document.createElement('p');
-palindromAnswer.classList.add('palindrom-form__answer');
-palindromInput.setAttribute('name', 'number');
-palindromInput.classList.add('palindrom-form__input');
-palindromSubmit.setAttribute('type', 'submit');
-palindromSubmit.classList.add('palindrom-form__submit');
-palindromSubmit.setAttribute('value', 'Submit');
-palindromForm.appendChild(palindromInput);
-palindromForm.appendChild(palindromSubmit);
-palindromForm.appendChild(palindromAnswer);
-
-palindromForm.addEventListener('submit', function(event) {
-	event.preventDefault();
-
-	if (palindromInput.value.length !== 5) {
-		palindromAnswer.innerText = 'Введите пятизначное число';
-		return;
-	}
-
-	const isPalindrom = palindromInput.value.split('').reverse().join('') === palindromInput.value;
-
-	//palindromAnswer.innerText = isPalindrom ? 'Палиндром' : 'Не палиндром';
-
-	// if (isPalindrom === true) {
-	// 	palindromAnswer.innerText = 'Палиндром';
-	// } else if (isPalindrom === false) {
-	// 	palindromAnswer.innerText = 'Не палиндром';
-	// }
-
-	if (isPalindrom) {
-		palindromAnswer.innerText = 'Палиндром';
-	} else {
-		palindromAnswer.innerText = 'Не палиндром';
-	}
-
-
+	console.log(arr);
 });
+
+document.addEventListener('click', () => {
+	console.dir(event.target);
+})
+
+
+
